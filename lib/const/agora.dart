4321272d786216401ff4d48121f28e20f5cb523c
편도훈 <pyeondohun@pyeondohun-ui-MacBookPro.local>
2024-01-09 11:11:08 +0900

    Initial commit

---
+++ lib/const/agora.dart
@@ -0,0 +1,6 @@
+const APP_ID = '7937998333124e3e92bd37a381b3698b';
+
+const CHANNEL_NAME = 'testtoken';
+
+const TEMP_TOKEN = '007eJxTYJDtu23SvE+h629Tb7vloXVezy3fXPMTc5wxLf3b5bTrz34qMJhbGptbWloYGxsbGpmkGqdaGiWlGJsnGlsYJhmbWVok+fnOSm0IZGQIyL3EzMgAgSA+J0NJanFJSX52ah4DAwB+vyMS';
+
---
+++ lib/screen/home_screen.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Center(
+            child: Text('HomeScreen'),
+          )
+        ],
+      ),
+    );
+  }
+}
